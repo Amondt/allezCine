6422175c8fb1ea76dad7741aeae865c3df18f24d
Amondt <mondt.alexande>
2019-04-02 10:57:43 +0200

    structure router / vue

--- allez-cine/src/main.js
+++ allez-cine/src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: App
+    },
+    {
+        path: '/:general',
+        name: 'general',
+        component: App
+    }
+]
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+})
+
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
