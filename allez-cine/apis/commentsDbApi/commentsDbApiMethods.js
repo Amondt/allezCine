9b08eb6b4ecde64ba5a70607aa647cb3d1a96ce5
Amondt <mondt.alexande>
2019-04-03 11:52:31 +0200

    commit

--- allez-cine/apis/commentsDbApi/commentsDbApiMethods.js
+++ allez-cine/apis/commentsDbApi/commentsDbApiMethods.js
@@ -1,5 +1,17 @@
+/* eslint-disable no-console */
 export function getComments (filmId) {
     let req = new XMLHttpRequest('GET', `localhost:8888/getdata.php?filmId=${filmId}`, false)
+    req.send(null)
+
+    if (req.status === 200) {
+        return JSON.parse(req.response)
+    } else {
+        return console.log("Status de la réponse: %d (%s)", req.status, req.statusText)
+    }
+}
+
+export function createComment (filmId, title, comment) {
+    let req = new XMLHttpRequest('GET', `localhost:8888/addComment.php?filmId=${filmId}&title=${title}&comment=${comment}`, false)
     req.send(null)
 
     if (req.status === 200) {
