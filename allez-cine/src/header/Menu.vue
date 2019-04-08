65053cf78650ebb2145cc732bba658fbbc421e42
Elisa Renda <elisa.renda@hotmail.fr>
2019-04-08 16:25:58 +0200

    hamburger

--- allez-cine/src/header/Menu.vue
+++ allez-cine/src/header/Menu.vue
@@ -1,11 +1,30 @@
 <template>
-    <div id="header"> 
-        <div class="navbar">
+    <div id="header">   
+        <a class="menu-toggle">
+            <span class="fa fa-bars"></span>
+        </a>
+        <div class="navbar" v-if="windowWidth > 500">
             <nav>
                 <router-link to='/home' id="home" :class="['home']">Home</router-link>
                 <router-link to='/films' id="films" :class="['films']">Films Populaires</router-link>
                 <router-link to='/series' id="series" :class="['series']">Séries TV</router-link>
             </nav>
+            <div class="search">
+                <input type="search" id="site-search" name="q"
+                aria-label="Search through site content" placeholder="Recherche">
+            </div>
+        </div>
+        <div class="navbar" v-else>
+            <div v-if="icon"> 
+                <i class="fas fa-bars" @click="menuClick"/>
+            </div>
+            <div v-else>
+                <nav>
+                    <router-link to='/home' id="home" :class="['home']">Home</router-link>
+                    <router-link to='/films' id="films" :class="['films']">Films Populaires</router-link>
+                    <router-link to='/series' id="series" :class="['series']">Séries TV</router-link>
+                </nav>
+            </div>
             <div class="search">
                 <input type="search" id="site-search" name="q"
                 aria-label="Search through site content" placeholder="Recherche">
@@ -20,7 +39,21 @@ import Carousel from '../components/Carousel.vue'
 
 export default {
     name: 'menuTop',
-    components: {Carousel}
+    data(){
+        return{
+            windowWidth: window.innerWidth,
+        }
+    },
+    components: {
+        Carousel
+    },
+mounted () {
+        window.onresize = () => {
+            this.windowWidth = window.innerWidth
+            console.log(this.windowWidth);
+        }
+},
+
 }
 </script>
 
@@ -188,6 +221,108 @@ button:hover {
   nav, .search {
     margin: 0;
   }
+  nav {
+    display:none;
+    }
+ 
 }
+
+.fa, .fas{
+    font-size: 30px;
+    position: absolute;
+    top:15px;
+    left: 13px;
+}
+
+/* wrapper styles */
+.wrapper {
+  position: relative;
+  min-height: 100vh;
+}
+
+/* small screens styles */
+@media (max-width: 767px) {
+  .sidebar {
+    position: absolute;
+    top: 0; left: 0; right: 0;
+  }
+  
+  /* navigation styles */
+  nav {
+    -webkit-columns: 2;
+    -moz-columns: 2;
+    -ms-columns: 2;
+    columns: 2;
+    -webkit-column-gap: 2px;
+    -moz-column-gap: 2px;
+    -ms-column-gap: 2px;
+    column-gap: 2px;
+    padding: 2px;
+  }
+  nav > router-link {
+    display: inline-block;
+    width: 100%;
+    height: 6rem;
+    line-height: 6rem;
+    padding-left: 1rem;
+  }
+  .wrapper {
+    transform: translateX(0) translateZ(0);
+    transition: transform .2s;
+    will-change: transform;
+  }  
+
+  /* button deco */
+  [class^="nav-button"] {
+    display: block;
+    z-index: 1;
+    height: 3.5rem; width: 3.5rem;
+    background-color: transparent;
+    background-image: linear-gradient(to right, #333, #333),
+      linear-gradient(to right, #333, #333),
+      linear-gradient(to right, #333, #333);
+    background-position: center top, center, center bottom;
+    background-repeat: no-repeat;
+    background-size: 3.5rem .9rem;
+    padding: 0;
+    outline: 0;
+    border: 0;
+    cursor: pointer;
+    -webkit-tap-highlight-color:rgba(0,0,0,0);
+  }
+  
+  /* here's goes the push top effect */
+  body:target .wrapper {
+    transform: translateY(19rem); /* based on navigation's height */
+  }
+  .nav-button-close,
+  body:target .nav-button-open {
+    display: none;
+  }
+  .nav-button-open,
+  body:target .nav-button-close {
+    display: block;
+  }
+  
+} /* end of small screen media queries */
+
+router-link {
+  color: #fff;
+}
+.wrapper {
+  padding: 2rem;
+  background-color: #eee;
+  color: #333;
+}
+.sidebar {
+  background: #333;
+  color: #ddd;
+}
+nav router-link {
+  margin-bottom: 2px;
+  text-decoration: none;
+  background: rgba(255,255,255, 0.1);
+}
+
 
 </style>
